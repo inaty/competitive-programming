de6e67996438f9ff92bab5deeb90376ddd8578a5
inaty <inaty0610@gmail.com>
2019-09-29 22:00:15 +0900

    add prime library

--- library/my_library.py
+++ library/my_library.py
@@ -1,3 +1,35 @@
+#nの素数判定
+def is_prime(n):
+    if n == 1:
+        return False
+    for i in range(2,int(n**0.5)+1):
+        if n % i == 0:
+            return False
+    return True
+
+
+#nの約数列挙
+def divisor(n):
+    ass = []
+    for i in range(1,int(n**0.5)+1):
+        if n%i == 0:
+            ass.append(i)
+            ass.append(n//i)
+    return ass #sortされていない
+
+
+#nの素因数分解(O(n**0.5)
+def prime_factor(n):
+    ass = []
+    for i in range(2,int(n**0.5)+1):
+        while n % i==0:
+            ass.append(i)
+            n = n//i
+    if n != 1:
+        ass.append(n)
+    return ass
+
+
 '''
 切り上げを行う関数
 参考(https://qiita.com/drken/items/fd4e5e3630d0f5859067#5-%E9%81%8E%E5%8E%BB%E5%95%8F%E7%B2%BE%E9%81%B8-10-%E5%95%8F)：
